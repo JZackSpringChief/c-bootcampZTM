19309563dd4bc85767f33c6d8e6f5f371f191114
Zack Spring Chief <jzacksc@gmail.com>
2023-11-12 11:18:00 -0700

    getting ride of bugs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, .NET!");
-Console.WriteLine("Kyle is a moron");
-//Console.WriteLine("Giving cringe");
-class Monster
-{
- public static void Monster 
-    {
-
-    }
-    
-}
-for (int 1 = 0; i < 10; i++)
-    {
-        Console.WriteLine(i);
-    }
