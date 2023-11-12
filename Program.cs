e4681169169214063f7309468ac203a4c088e656
Zack Spring Chief <jzacksc@gmail.com>
2023-11-11 22:07:35 -0700

    testing class and constructor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, .NET!");
 Console.WriteLine("Kyle is a moron");
-Console.WriteLine("Giving cringe");
+//Console.WriteLine("Giving cringe");
+class Monster
+{
+ public static void Monster 
+    {
+        
+    }
+}
