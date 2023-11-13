5fb83294c2c6bb75b0a9c47a691cda681ac2634e
Zack Spring Chief <jzacksc@gmail.com>
2023-11-12 17:33:29 -0700

    testing other value types

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace test
+﻿using System.Net.Sockets;
+
+namespace test
 {
     internal class Program
     {
@@ -12,6 +14,29 @@
             Console.WriteLine($"bool: {enabled}");
             enabled = true;
             Console.WriteLine($"bool: {enabled}");
+
+            char a = 'A';
+            Console.WriteLine($"char: {a}");
+
+            int q = 5;
+            int r = 6;
+            int s = -4;
+
+            Console.WriteLine($"q: {q}, r: {r}, s: {s}");
+
+            int multiply = s * r;
+            int divide = r / s;
+            int add = q + r;
+            int sub = r - s;
+            Console.WriteLine($"multiply: {multiply}, divide: {divide}, add: {add}, subtract: {sub}");
+
+            long n = 10203933874;
+            Console.WriteLine($"long: {n}");
+
+            float z = 3.5f;
+            double x = 7.8d;
+            decimal dec = 7.15m;
+            Console.WriteLine($"float: {z}, double: {x}, decimal: {dec}");
         }
     }
 }
