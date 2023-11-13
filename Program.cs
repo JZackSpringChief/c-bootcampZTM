01adb68a95ed4aca3d54ba3e19ce1359ddb08e95
Zack Spring Chief <jzacksc@gmail.com>
2023-11-12 17:54:03 -0700

    casting

--- Program.cs
+++ Program.cs
@@ -10,44 +10,60 @@ namespace test
             // string? name = Console.ReadLine();
 
             // Console.WriteLine($"Hello, {name}");
-            bool enabled = false;
-            Console.WriteLine($"bool: {enabled}");
-            enabled = true;
-            Console.WriteLine($"bool: {enabled}");
+            // bool enabled = false;
+            // Console.WriteLine($"bool: {enabled}");
+            // enabled = true;
+            // Console.WriteLine($"bool: {enabled}");
 
-            char a = 'A';
-            Console.WriteLine($"char: {a}");
+            // char a = 'A';
+            // Console.WriteLine($"char: {a}");
 
-            int q = 5;
-            int r = 6;
-            int s = -4;
+            // int q = 5;
+            // int r = 6;
+            // int s = -4;
 
-            Console.WriteLine($"q: {q}, r: {r}, s: {s}");
+            // Console.WriteLine($"q: {q}, r: {r}, s: {s}");
 
-            int multiply = s * r;
-            int divide = r / s;
-            int add = q + r;
-            int sub = r - s;
-            Console.WriteLine($"multiply: {multiply}, divide: {divide}, add: {add}, subtract: {sub}");
+            // int multiply = s * r;
+            // int divide = r / s;
+            // int add = q + r;
+            // int sub = r - s;
+            // Console.WriteLine($"multiply: {multiply}, divide: {divide}, add: {add}, subtract: {sub}");
 
-            long n = 10203933874;
-            Console.WriteLine($"long: {n}");
+            // long n = 10203933874;
+            // Console.WriteLine($"long: {n}");
 
-            float z = 3.5f;
-            double x = 7.8d;
-            decimal dec = 7.15m;
-            Console.WriteLine($"float: {z}, double: {x}, decimal: {dec}");
+            // float z = 3.5f;
+            // double x = 7.8d;
+            // decimal dec = 7.15m;
+            // Console.WriteLine($"float: {z}, double: {x}, decimal: {dec}");
 
-            //copying values;
-            int a1 = 7;
-            int a2 = a1;
-            a1 = 4;
-            Console.WriteLine($"a1: {a1}, a2: {a2}");
+            // //copying values;
+            // int a1 = 7;
+            // int a2 = a1;
+            // a1 = 4;
+            // Console.WriteLine($"a1: {a1}, a2: {a2}");
 
-            //Nullable value types;
-            int? aa = null;
-            bool? bb = null;
-            Console.WriteLine($"int?: {aa}, bool?: {bb}");
+            // //Nullable value types;
+            // int? aa = null;
+            // bool? bb = null;
+            // Console.WriteLine($"int?: {aa}, bool?: {bb}");
+
+            //implicit vs explicit
+            int age = 33;
+            float weight = 72.3f;
+            Console.WriteLine($"age: {age}, Weight: {weight}");
+
+            //implicit
+            long myAge = age;
+
+            //explicit;
+            int lessWeight = (int)weight;
+                                //this is casting...
+
+            long bigNum = 1111111111111111111;
+            int smallNum = (int)bigNum;
+            Console.WriteLine(smallNum);
         }
     }
 }
