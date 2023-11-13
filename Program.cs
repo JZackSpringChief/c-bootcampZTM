2a590a0c91f6e88bd39a5a9c4c28a8c001a58fa9
Zack Spring Chief <jzacksc@gmail.com>
2023-11-12 17:23:02 -0700

    testing value type bool

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter your Name: ");
-            string? name = Console.ReadLine();
+            // Console.WriteLine("Enter your Name: ");
+            // string? name = Console.ReadLine();
 
-            Console.WriteLine($"Hello, {name}");
+            // Console.WriteLine($"Hello, {name}");
+            bool enabled = false;
+            Console.WriteLine($"bool: {enabled}");
+            enabled = true;
+            Console.WriteLine($"bool: {enabled}");
         }
     }
 }
