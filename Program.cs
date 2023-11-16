e6207443f1425e39f6a7b2779711b78c3e70629c
Zack Spring Chief <jzacksc@gmail.com>
2023-11-15 20:28:37 -0700

    adding

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 ﻿Person p1 = new();
 Person p2 = new();
 
+p1.Name = "Kyle";
+Console.WriteLine(p1.Name);
 
 class Person
 {
+    private string name;
+    public Person()
+    {
+        name = "zack";
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
 
 }
