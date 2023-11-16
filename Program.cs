93ccb68ae20835451ab1434ffb667f51b87d392c
Zack Spring Chief <jzacksc@gmail.com>
2023-11-15 20:45:47 -0700

    doing my own class

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ Person p1 = new("zack", 33, false);
 Person p2 = new("kyle", 31, true);
 Person p3 = new("Daniel", 35, false);
 
+Hero h1 = new("Blair", 9999, 9999);
+
 Console.WriteLine(p1.Name);
 Console.WriteLine(p2.Age);
 Console.WriteLine(p3.InSchool);
+
+Console.WriteLine($"Hero Name: {h1.Name}. Hero's Attack: OVER {h1.Attack}. Hero's Defense: {h1.Defense}.");
+
 
 class Person
 {
@@ -20,4 +25,16 @@ class Person
     public string Name { get; private set; }
     public int Age { get; private set; }
     public bool InSchool { get; private set; }
+}
+class Hero
+{
+    public Hero(string name, int attack, int defense)
+    {
+        Name = name;
+        Attack = attack;
+        Defense = defense;
+    }
+    public string Name { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
 }
