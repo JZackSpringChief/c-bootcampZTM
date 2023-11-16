b122ca6a4722d3912642189eb257bb2feaa55c01
Zack Spring Chief <jzacksc@gmail.com>
2023-11-15 20:39:34 -0700

    playing around with properties and objects

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
-﻿Person p1 = new("zack");
-Person p2 = new("kyle");
+﻿using System.Security.AccessControl;
+
+Person p1 = new("zack", 33, false);
+Person p2 = new("kyle", 31, true);
+Person p3 = new("Daniel", 35, false);
 
 Console.WriteLine(p1.Name);
+Console.WriteLine(p2.Age);
+Console.WriteLine(p3.InSchool);
 
 class Person
 {
-    public Person(string name)
+    public Person(string name, int age, bool inschool)
     {
         Name = name;
+        Age = age;
+        InSchool = inschool;
     }
 
     public string Name { get; private set; }
+    public int Age { get; private set; }
+    public bool InSchool { get; private set; }
 }
