a96475cf6354bb1770c5a907c2b509dc753fb7f4
Zack Spring Chief <jzacksc@gmail.com>
2023-11-17 14:27:54 -0700

    learining

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 ﻿//Inheritance
+using System.Reflection.Metadata.Ecma335;
+
 Person p1 = new("Aric");
 Employee p2 = new("Zack", "CEO");
 
 Console.WriteLine($"{p1.Name}: {p1.GetId()}");
 Console.WriteLine($"{p2.Name}: {p2.GetId()}");
+
+public interface ISalary
+{
+    decimal GetSalary();
+}
+//can use be set as public "sealed" class Person to prevent inheritance
 public class Person
 {
     public string Name { get; set; }
@@ -11,17 +19,26 @@ public class Person
     {
         Name = name;
     }
-    public string GetId()
+    public virtual string GetId()
     {
         return Name.ToLower();
     }
 }
 
-public class Employee : Person
+public class Employee : Person, ISalary
 {
     public string Role { get; set; }
     public Employee(string name, string role) : base(name)
     {
         Role = role;
+    }
+    public override string GetId()
+    {
+        return $"{Role.ToLower()}-{base.GetId()}";
+    }
+
+    public decimal GetSalary()
+    {
+        return 6000;
     }
 }
