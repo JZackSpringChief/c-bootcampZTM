684004433efcb4661851eb5f48633f5e93c56677
Zack Spring Chief <jzacksc@gmail.com>
2023-11-16 21:32:04 -0700

    static members

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿using Namespaces.Data;
 Person person = new();
+int result = SuperMath.Sum(10, 32);
+Console.WriteLine(result);
+Console.WriteLine(SuperMath.PI);
+public static class SuperMath
+{
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+    public static double PI
+    {
+        get { return 3.14734d; }
+    }
+}
