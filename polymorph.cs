79b09e49a59c016ed61395f9ad72d17bf91fb755
Zack Spring Chief <jzacksc@gmail.com>
2023-11-19 19:19:03 -0700

    time for question

--- polymorph.cs
+++ polymorph.cs
@@ -42,3 +42,35 @@
 //         return 6000;
 //     }
 // }
+
+// Person p1 = new();
+// Person p2 = new Employee();
+// Person p3 = new Customer();
+
+// p1.Greeting();
+// p2.Greeting();
+// p3.Greeting();
+
+
+
+// public class Person
+// {
+//     public virtual void Greeting()
+//     {
+//         Console.WriteLine("I am a reg");
+//     }
+// }
+// public class Employee : Person
+// {
+//     public override void Greeting()
+//     {
+//         Console.WriteLine("I am an Employee");
+//     }
+// }
+// public class Customer : Person
+// {
+//     public override void Greeting()
+//     {
+//         Console.WriteLine("I am a Customer");
+//     }
+// }
