de54824968d785c87b61e24ad44ade4271e9de14
Zack Spring Chief <jzacksc@gmail.com>
2023-11-19 19:25:53 -0700

    accounting question

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//greet user with "Welcome to the Accounting System 1.0"
+Console.WriteLine("Welcome to the Accounting System 1.0");
+
+//The accounting system should have a checking account and a premium account;
+
+//a checking account has an ID(number) and a balance(decimal number);
+
+//You should be able to set a starting balance when creating a checking account;
+
+// you should be able to calculate interest based on the current balance of the checking account;
+
+// you should be able to calculate interest based on the current balance of the checking account and an input
+
+//you should be able to print the ID and the current blanace of a checking account at anytime;
+
+//the premimun account has the same behavior as the checking account.
+//The only difference is that tha premium account gets a 1% extra interest on top of the user input;
+
+//you should be able to transfer money from the one account to another at any time;
+
+//
