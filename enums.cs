9cb045d635dec50a0001fb9ccf1fab7a46800870
Zack Spring Chief <jzacksc@gmail.com>
2023-11-22 13:33:40 -0700

    adding enums

---
+++ enums.cs
@@ -0,0 +1,19 @@
+// enum Days
+// {
+//     Sunday,
+//     Monday,
+//     Tuesday,
+//     Wednesday,
+//     Thursday,
+//     Friday,
+//     Saturday
+// }
+// class Enums
+// {
+//     static void Main()
+//     {
+//         Days today = Days.Wednesday;
+//         Console.WriteLine(today);
+
+//     }
+// }
