1909e3968806159b24a26704813fead3e267ea50
Zack Spring Chief <jzacksc@gmail.com>
2023-11-23 14:55:21 -0700

    adding Generic Queue

--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
-﻿Race race1 = new("FastBoi", 11 / 22 / 2023, "Turtle");
-//Console.WriteLine();
+﻿Race race1 = new("FastBoi", "11 / 24 / 2023", "Turtle");
+Race race2 = new("FasterBoi", "11 / 30 / 2023", "Hare");
+Race race3 = new("FastestBoi", "12 / 02 / 2023", "Godzilla");
+//Console.WriteLine(race1.Date);
 
 
 var racesCalendar = new Queue<string>();
-racesCalendar.Enqueue($"Race scheduled: Race Name: {race1.Name} Date: {race1.Date} Trackname: {race1.TrackName}");
-racesCalendar.Enqueue("Race scheduled: November 30th @ 7:00pm");
-racesCalendar.Enqueue("Race scheduled: December 2nd @ 9:00am");
+racesCalendar.Enqueue($"Race scheduled 'Race Name: {race1.Name} Date: {race1.Date} Trackname: {race1.TrackName}'");
+racesCalendar.Enqueue($"Race scheduled 'Race Name: {race2.Name} Date: {race2.Date} Trackname: {race2.TrackName}'");
+racesCalendar.Enqueue($"Race scheduled 'Race Name: {race3.Name} Date: {race3.Date} Trackname: {race3.TrackName}'");
 
-
-
-
+while (racesCalendar.Count > 0)
+{
+    var raceCalender = racesCalendar.Dequeue();
+    Console.WriteLine(raceCalender);
+}
 
 public class Race
 {
-    public Race(string name, int date, string trackName)
+    public Race(string name, string date, string trackName)
     {
         Name = name;
         Date = date;
         TrackName = trackName;
     }
     public string Name { get; set; }
-    public int Date { get; set; }
+    public string Date { get; set; }
     public string TrackName { get; set; }
 }
