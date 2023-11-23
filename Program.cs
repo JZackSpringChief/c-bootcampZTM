13037f37ea3a2d415f84bd99fc433351394652b1
Zack Spring Chief <jzacksc@gmail.com>
2023-11-23 14:44:25 -0700

    Console Based Racing Calender

--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
-﻿//generic dictionaary;
-var grades = new Dictionary<char, int>
+﻿Race race1 = new("FastBoi", 11 / 22 / 2023, "Turtle");
+//Console.WriteLine();
+
+
+var racesCalendar = new Queue<string>();
+racesCalendar.Enqueue($"Race scheduled: Race Name: {race1.Name} Date: {race1.Date} Trackname: {race1.TrackName}");
+racesCalendar.Enqueue("Race scheduled: November 30th @ 7:00pm");
+racesCalendar.Enqueue("Race scheduled: December 2nd @ 9:00am");
+
+
+
+
+
+public class Race
 {
-    { 'A', 100 },
-    { 'B', 85}
-};
-
-grades.Add('C', 70);
-grades['D'] = 65;
-grades['E'] = 50;
-grades['F'] = 49;
-
-Console.WriteLine(grades['E']);
-
-foreach (var grade in grades)
-{
-    Console.WriteLine($"{grade.Key}: {grade.Value}");
-
+    public Race(string name, int date, string trackName)
+    {
+        Name = name;
+        Date = date;
+        TrackName = trackName;
+    }
+    public string Name { get; set; }
+    public int Date { get; set; }
+    public string TrackName { get; set; }
 }
-Console.WriteLine(grades.ContainsKey('F'));
