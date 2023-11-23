c8229a9b3f1bbce00b98ff5e6895e5aea0949f35
Zack Spring Chief <jzacksc@gmail.com>
2023-11-22 21:47:33 -0700

    arrays

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
-﻿Days today = Days.Wednesday;
-Console.WriteLine(today);
-enum Days
-{
-    Sunday,
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday
-}
+﻿int[] array1 = new int[4];
+array1[0] = 7;
+array1[1] = 14;
+array1[2] = 21;
+array1[3] = 14;
 
-
-
-
+Console.WriteLine(array1[3]);
