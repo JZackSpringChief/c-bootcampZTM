282913494b7a44e9da422dcb8538b72bd316c28b
Zack Spring Chief <jzacksc@gmail.com>
2023-11-25 18:33:27 -0700

    extention class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿//extention methods
+var p1 = new Person("Zack", "Spring Chief");
+Console.WriteLine(p1.ToFullName());
+
+public static class PersonExtention
+{
+    public static string ToFullName(this Person p1)
+    {
+        return $"{p1.FirstName} {p1.LastName}";
+    }
+}
+
+public class Person
+{
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
