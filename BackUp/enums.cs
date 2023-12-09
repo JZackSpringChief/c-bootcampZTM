d028b872a36619328cc40f9435cef159e2cfa5c2
Zack Spring Chief <jzacksc@gmail.com>
2023-12-09 15:02:42 -0700

    garbage collection

--- BackUp/enums.cs
+++ BackUp/enums.cs
@@ -54,3 +54,37 @@
 //         }
 //     }
 // }
+//Switch Expression
+// var temperature = new Temperature(TempuratureUnit.Celcius, 112.6m);
+// Console.WriteLine($"Tempurature: {temperature.ValueInCelcius.ToString("0.0")}C");
+// enum TempuratureUnit
+// {
+//     Celcius,
+//     Fahrenheit,
+//     Kelvin
+// }
+
+// class Temperature
+// {
+//     public Temperature(TempuratureUnit unit, decimal value)
+//     {
+//         Unit = unit;
+//         Value = value;
+//     }
+//     public TempuratureUnit Unit { get; set; }
+//     public decimal Value { get; set; }
+//     public decimal ValueInCelcius
+//     {
+//         get
+//         {
+//             return Unit switch
+//             {
+//                 TempuratureUnit.Celcius when Value > 100 => Math.Round(Value, 0), //guard clause
+//                 TempuratureUnit.Celcius => Value,
+//                 TempuratureUnit.Fahrenheit => (Value - 32) * 5 / 9,
+//                 TempuratureUnit.Kelvin => Value - 273.5m,
+//                 _ => 0
+//             };
+//         }
+//     }
+// }
