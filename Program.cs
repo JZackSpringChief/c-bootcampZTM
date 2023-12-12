81ffd4a54f18c3e1f307faff0bb9ac3eeeaec9dc
Zack Spring Chief <jzacksc@gmail.com>
2023-12-11 21:39:40 -0700

    generic list

--- Program.cs
+++ Program.cs
@@ -45,32 +45,3 @@
 //     string Name { get; set; }
 // }
 
-var numbers = new List<int>();
-var day = 22;
-numbers.Add(day);
-
-foreach (var number in numbers)
-{
-    Console.WriteLine($"Number: {number}");
-}
-
-var persons = new List<Person>();
-var person1 = new Person("Zack");
-var person2 = new Person("Kyle");
-var person3 = new Person("Daniel");
-persons.Add(person1);
-persons.Add(person2);
-persons.Add(person3);
-foreach (var person in persons)
-{
-    Console.WriteLine($"Person: {person.Name}");
-}
-Console.WriteLine(persons[2].Name);
-public class Person
-{
-    public Person(string name)
-    {
-        Name = name;
-    }
-    public string Name { get; set; }
-}
