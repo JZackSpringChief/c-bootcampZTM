9acf5070eb59e92fa8674c641aaba34d33425ff1
Zack Spring Chief <jzacksc@gmail.com>
2023-12-13 14:24:51 -0700

    adding drivers .cs file

--- Program.cs
+++ Program.cs
@@ -1,36 +1,12 @@
-﻿Race race1 = new("FastBoi", date: "11 / 24 / 2023", trackname: "Turtle");
-Race race2 = new("FasterBoi", "11 / 30 / 2023", "Hare");
-Race race3 = new("FastestBoi", "12 / 02 / 2023", "Godzilla");
+﻿Race race1 = new(racename: "FastBoi", date: "11 / 24 / 2023", trackname: "Turtle");
+Race race2 = new(racename: "FasterBoi", date: "11 / 30 / 2023", trackname: "Hare");
+Race race3 = new(racename: "FastestBoi", date: "12 / 02 / 2023", trackname: "Godzilla");
 //Console.WriteLine(race1.Date);
 var racesCalendar = new Queue<string>();
 racesCalendar.Enqueue($"Race scheduled 'Race Name: {race1.RaceName} Date: {race1.Date} Trackname: {race1.TrackName}'");
 racesCalendar.Enqueue($"Race scheduled 'Race Name: {race2.RaceName} Date: {race2.Date} Trackname: {race2.TrackName}'");
 racesCalendar.Enqueue($"Race scheduled 'Race Name: {race3.RaceName} Date: {race3.Date} Trackname: {race3.TrackName}'");
 
-Driver d1 = new("Zack");
-Driver d2 = new("Kyle");
-Driver d3 = new("Daniel");
-Driver d4 = new("Dad");
-Driver d5 = new("Nicole");
-Driver d6 = new("Donnie");
-Driver d7 = new("Janna");
-Driver d8 = new("Kylie");
-Driver d9 = new("Cazz");
-Driver d10 = new("Meme");
-Driver d11 = new("Emma");
-Driver d12 = new("Matao");
-Driver d13 = new("Abby");
-Driver d14 = new("Logan");
-Driver d15 = new("Nargy");
-Driver d16 = new("Cade");
-Driver d17 = new("Katie");
-Driver d18 = new("Joseph");
-Driver d19 = new("Tristan");
-Driver d20 = new("Blair");
-Driver d21 = new("Lance");
-Driver d22 = new("Tim");
-Driver d23 = new("Tom");
-//23 drivers
 while (racesCalendar.Count > 0)
 {
     var raceCalender = racesCalendar.Dequeue();
@@ -47,14 +23,14 @@ public class Race
     public string RaceName { get; set; }
     public string Date { get; set; }
     public string TrackName { get; set; }
-    public int AddDriver()
+    public static void AddDriver()
     {
-        int Drivers = 0;
-        while (Drivers <= 20)
+        var NewDriver = new Queue<string>();
+        NewDriver.Enqueue("Zack");
+        foreach (var driver in NewDriver)
         {
-            Drivers += 1;
+            Console.WriteLine(driver);
         }
-        return Drivers;
     }
 }
 public class Driver
---
+++ drivers.cs
@@ -0,0 +1,24 @@
+// Driver d1 = new("Zack");
+// Driver d2 = new("Kyle");
+// Driver d3 = new("Daniel");
+// Driver d4 = new("Dad");
+// Driver d5 = new("Nicole");
+// Driver d6 = new("Donnie");
+// Driver d7 = new("Janna");
+// Driver d8 = new("Kylie");
+// Driver d9 = new("Cazz");
+// Driver d10 = new("Meme");
+// Driver d11 = new("Emma");
+// Driver d12 = new("Matao");
+// Driver d13 = new("Abby");
+// Driver d14 = new("Logan");
+// Driver d15 = new("Nargy");
+// Driver d16 = new("Cade");
+// Driver d17 = new("Katie");
+// Driver d18 = new("Joseph");
+// Driver d19 = new("Tristan");
+// Driver d20 = new("Blair");
+// Driver d21 = new("Lance");
+// Driver d22 = new("Tim");
+// Driver d23 = new("Tom");
+//23 drivers
