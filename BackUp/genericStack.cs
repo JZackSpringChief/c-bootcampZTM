bb7efc86bd08e6700172b205a6863677ba911af6
Zack Spring Chief <jzacksc@gmail.com>
2023-12-12 20:05:12 -0700

    generic stack

---
+++ BackUp/genericStack.cs
@@ -0,0 +1,18 @@
+// var countries = new Stack<string>();
+
+// countries.Push("Japan");
+// countries.Push("Poland");
+// countries.Push("Germany");
+// countries.Push("Ireland");
+// countries.Push("USA");
+
+// var lastItem = countries.Peek();
+// Console.WriteLine(lastItem);
+// countries.Pop();
+
+// countries.Contains("Canada");
+
+// foreach (var country in countries)
+// {
+//     Console.WriteLine(country);
+// }
