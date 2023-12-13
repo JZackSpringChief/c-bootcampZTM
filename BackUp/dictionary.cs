4243f8646bfe582cd4791388814499ab7c32482c
Zack Spring Chief <jzacksc@gmail.com>
2023-12-12 20:28:09 -0700

    dictionary list

---
+++ BackUp/dictionary.cs
@@ -0,0 +1,15 @@
+// var grades = new Dictionary<int, char>
+// {
+//     {90, 'A'},
+//     {80, 'B'},
+// };
+// grades.Add(70, 'C');
+// grades[60] = 'D';
+// grades[50] = 'F';
+
+// Console.WriteLine(grades[70]);
+
+// foreach (var grade in grades)
+// {
+//     Console.WriteLine($"{grade.Key}: {grade.Value}");
+// }
--- Program.cs
+++ Program.cs
@@ -40,4 +40,18 @@
 //     }
 //     string Name { get; set; }
 // }
+var grades = new Dictionary<int, char>
+{
+    {90, 'A'},
+    {80, 'B'},
+};
+grades.Add(70, 'C');
+grades[60] = 'D';
+grades[50] = 'F';
 
+Console.WriteLine(grades[70]);
+
+foreach (var grade in grades)
+{
+    Console.WriteLine($"{grade.Key}: {grade.Value}");
+}
